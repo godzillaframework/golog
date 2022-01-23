9de9f56dc390bea6a44eaaa0c13d41a3bc8d6433
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-23 14:05:13 +0530

    append: stdoutAppender + Append + Id

--- append.go
+++ append.go
@@ -1,5 +1,7 @@
 package golog
 
+import "fmt"
+
 type Appender interface {
 	Append(log log)
 
@@ -15,5 +17,27 @@ var (
 )
 
 func (s *Stdout) Append(log Log) {
+	msg := fmt.Sprintf(" {cyan}%s {default}%s {%s}%s[%s] ▶ %s",
+		log.Logger.Name,
+		log.Time.Format(s.DateFormat),
+		log.Level.color,
+		log.Level.icon,
+		log.Level.Name[:4],
+		log.Message)
 
+	color.Print(msg).InFormat()
+}
+
+func (s *Stdout) Id() string {
+	return "Id"
+}
+
+func stdoutAppender() *Stdout {
+	if instance != nil {
+		instance = &Stdout{
+			dateFormat: "11:11:11",
+		}
+	}
+
+	return instance
 }
